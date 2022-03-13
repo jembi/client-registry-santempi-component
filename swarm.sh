29b4d4a7024cba2e797fbe418520cff0de0c6a8a
MatthewErispe <matthew.frc@hotmail.com>
2022-03-13 19:43:39 +0200

    Update SanteMPI files for the platform

--- swarm.sh
+++ swarm.sh
@@ -6,25 +6,25 @@ composeFilePath=$(
 
 if [ "$2" == "dev" ]; then
   printf "\nRunning Client Registry SanteMPI package in DEV mode\n"
-  santeDevComposeParam="-c ${composeFilePath}/docker-compose.dev.yml"
+  santeMpiDevComposeParam="-c ${composeFilePath}/docker-compose.dev.yml"
 else
   printf "\nRunning Client Registry SanteMPI package in PROD mode\n"
-  santeDevComposeParam=""
+  santeMpiDevComposeParam=""
 fi
 
 if [ "$1" == "init" ]; then
-  docker stack deploy -c "$composeFilePath"/docker-compose.yml $santeDevComposeParam instant
+  docker stack deploy -c "$composeFilePath"/docker-compose.yml $santeMpiDevComposeParam instant
 elif [ "$1" == "up" ]; then
-  docker stack deploy -c "$composeFilePath"/docker-compose.yml $santeDevComposeParam instant
+  docker stack deploy -c "$composeFilePath"/docker-compose.yml $santeMpiDevComposeParam instant
 elif [ "$1" == "down" ]; then
-  docker service scale instant_postgres-santedb=0 instant_sante-mpi=0 instant_sante-mpi-console=0
+  docker service scale instant_santedb-postgres=0 instant_santedb-mpi=0 instant_santedb-www=0
 elif [ "$1" == "destroy" ]; then
-  docker service rm instant_postgres-santedb instant_sante-mpi instant_sante-mpi-console
+  docker service rm instant_santedb-postgres instant_santedb-mpi instant_santedb-www
 
   echo "Sleep 10 Seconds to allow services to shut down before deleting volumes"
   sleep 10
 
-  docker volume rm instant_santedb-data instant_sante-postgres-data
+  docker volume rm instant_santedb-data instant_santedb-postgres-data
 
   if [ $statefulNodes == "cluster" ]; then
     echo "Volumes are only deleted on the host on which the command is run. Mongo volumes on other nodes are not deleted"
